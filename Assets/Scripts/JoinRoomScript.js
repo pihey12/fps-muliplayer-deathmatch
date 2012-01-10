a62210fb6a233436da93eb2e6b6e47a31b4735a6
prophetster@gmail.com <prophetster@gmail.com@52c63998-15eb-d182-d11c-520bc6e810c1>
2012-01-10 04:55:24 +0000

    Menu framework is in place!

--- Assets/Scripts/JoinRoomScript.js
+++ Assets/Scripts/JoinRoomScript.js
@@ -1,3 +1,34 @@
+var newSkin : GUISkin;
+var logoTexture : Texture2D;
 
-function Update () {
+function theFirstMenu() {
+	var script;
+    //layout start
+    GUI.BeginGroup(Rect(20, 0, 350, Screen.height));
+    
+    //the menu background box
+    GUI.Box(Rect(0, 0, 300, 200), "");
+    
+    //logo picture
+    GUI.Label(Rect(15, 10, 300, 68), logoTexture);
+    
+    ///////main menu buttons
+    //game start button
+    if(GUI.Button(Rect(5, Screen.height-50, 300, 40), "Back")) {
+    script = GetComponent("MainMenuScript"); 
+    script.enabled = true;
+    var script2 = GetComponent("JoinRoomScript"); 
+    script2.enabled = false;
+    }    
+    
+    //layout end
+    GUI.EndGroup(); 
+}
+
+function OnGUI () {
+    //load GUI skin
+    GUI.skin = newSkin;
+    
+    //execute theFirstMenu function
+    theFirstMenu();
 }
