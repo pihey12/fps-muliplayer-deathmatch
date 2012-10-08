95e525ecb9d6c00b7ce106a582ce54e6a25b50e7
prophetster@gmail.com <prophetster@gmail.com@52c63998-15eb-d182-d11c-520bc6e810c1>
2012-10-08 05:48:50 +0000

    More character creation stuff

--- Assets/Scripts/CreateCharacterScript.js
+++ Assets/Scripts/CreateCharacterScript.js
@@ -2,33 +2,34 @@ var newSkin : GUISkin;
 var logoTexture : Texture2D;
 var headSelection : int = 0;
 var prevHeadSelect : int = 1;
-
+var headColor : int=0;
 var torsoSelection : int = 1;
 var prevTorsoSelect : int = 1;
-
+var bodyColor : int = 0;
 var ruArmSelection : int = 1;
 var prevRUArmSelect : int = 1;
-
+var ruArmColor : int = 0;
 var rlArmSelection : int = 1;
 var prevRLArmSelect : int = 1;
-
+var rlArmColor : int = 0;
 var luArmSelection : int = 1;
 var prevLUArmSelect : int = 1;
-
+var luArmColor : int = 0;
 var llArmSelection : int = 1;
 var prevLLArmSelect : int = 1;
-
+var llArmColor : int = 0;
 var ruLegSelection : int = 1;
 var prevRULegSelect : int = 1;
-
+var ruLegColor : int = 0;
 var rlLegSelection : int = 1;
 var prevRLLegSelect : int = 1;
-
+var rlLegColor : int = 0;
 var luLegSelection : int = 1;
 var prevLULegSelect : int = 1;
-
+var luLegColor : int = 0;
 var llLegSelection : int = 1;
 var prevLLLegSelect : int = 1;
+var llLegColor : int = 0;
 
 var baseCharacter : GameObject;
 baseCharacter = GameObject.Find("baseCharacter");
@@ -206,6 +207,11 @@ while(count <= 6)
 	prevLLLegSelect = count;
 	count++;
 }
+var testMaterial : Material;
+var colorCounter = 0;
+//testMaterial = Resources.Load("bodyParts/Materials/body");
+//testMaterial.SetColor("_Color",Color.green);
+//torsoObject[0].material.SetColor("_SpecColor",Color.red);
 
 function createCharacter() {
 	GUI.skin = newSkin;
@@ -215,7 +221,7 @@ function createCharacter() {
 	GUI.Label(Rect(5,5,300,50),"Character Creation");
 	
 	//Grouping for head and body--------------------
-	GUI.BeginGroup(Rect(20, Screen.height/12, 280, 60));
+	GUI.BeginGroup(Rect(20, Screen.height/12, 280, 90));
     //Head selection
     GUI.Label(Rect(40,0, 80, 30),"Head " + headSelection.ToString());
     headSelection = GUI.HorizontalScrollbar (Rect(0,30, 120, 30),headSelection, 1, 1, 10);
@@ -224,7 +230,22 @@ function createCharacter() {
     	tempPart = tempHead;
     	updateBody(headSelection,headObject,prevHeadSelect);
     	prevHeadSelect = headSelection;
-    }        
+    }
+    //Head Color
+    if(GUI.Button(Rect(0, 50, 120, 30), "Head Color")) 
+    {
+    	testMaterial = Resources.Load("bodyParts/Materials/head");
+    	if(headColor <= 9)
+    	{
+    		updateColor(testMaterial,headColor);
+    	}
+    	else
+    	{
+    		headColor = 0;
+    		updateColor(testMaterial,headColor);
+    	}
+    	headColor++;
+    }
     //Torso selection
     GUI.Label(Rect(170,0, 80, 30),"Torso " + torsoSelection.ToString());
     torsoSelection = GUI.HorizontalScrollbar (Rect(130,30, 120, 30),torsoSelection, 1, 1, 8);
@@ -234,10 +255,25 @@ function createCharacter() {
     	updateBody(torsoSelection,torsoObject,prevTorsoSelect);
     	prevTorsoSelect = torsoSelection;
     }
+    //Body Color
+    if(GUI.Button(Rect(130, 50, 120, 30), "Body Color")) 
+    {
+    	testMaterial = Resources.Load("bodyParts/Materials/body");
+    	if(bodyColor <= 9)
+    	{
+    		updateColor(testMaterial,bodyColor);
+    	}
+    	else
+    	{
+    		bodyColor = 0;
+    		updateColor(testMaterial,bodyColor);
+    	}
+    	bodyColor++;
+    }
     GUI.EndGroup();
     
     //Grouping for right arm-------------------------------
-    GUI.BeginGroup(Rect(150, Screen.height/5.5, 130, 110));
+    GUI.BeginGroup(Rect(150, Screen.height/4.5, 130, 180));
     //Right upper arm
     GUI.Label(Rect(0,0, 120, 30),"Upper Right Arm " + ruArmSelection.ToString());
     ruArmSelection = GUI.HorizontalScrollbar (Rect(0,30, 120, 30),ruArmSelection, 1, 1, 8);
@@ -247,19 +283,48 @@ function createCharacter() {
     	updateBody(ruArmSelection,ruArmObject,prevRUArmSelect);
     	prevRUArmSelect = ruArmSelection;
     }
+    // Right upper arm color
+    if(GUI.Button(Rect(0, 50, 120, 30), "Upper Arm Color")) 
+    {
+    	testMaterial = Resources.Load("bodyParts/Materials/ruArm");
+    	if(ruArmColor <= 9)
+    	{
+    		updateColor(testMaterial,ruArmColor);
+    	}
+    	else
+    	{
+    		ruArmColor = 0;
+    		updateColor(testMaterial,ruArmColor);
+    	}
+    	ruArmColor++;
+    }
     //Right lower arm
-    GUI.Label(Rect(0,60, 120, 30),"Lower Right Arm " + rlArmSelection.ToString());
-    rlArmSelection = GUI.HorizontalScrollbar (Rect(0,90, 120, 30),rlArmSelection, 1, 1, 8);
+    GUI.Label(Rect(0,80, 120, 30),"Lower Right Arm " + rlArmSelection.ToString());
+    rlArmSelection = GUI.HorizontalScrollbar (Rect(0,110, 120, 30),rlArmSelection, 1, 1, 8);
     if(rlArmSelection != prevRLArmSelect)
     {
     	tempPart = tempRLowerArm;
     	updateBody(rlArmSelection,rlArmObject,prevRLArmSelect);
     	prevRLArmSelect = rlArmSelection;
     }
+    if(GUI.Button(Rect(0, 130, 120, 30), "Lower Arm Color")) 
+    {
+    	testMaterial = Resources.Load("bodyParts/Materials/rlArm");
+    	if(rlArmColor <= 9)
+    	{
+    		updateColor(testMaterial,rlArmColor);
+    	}
+    	else
+    	{
+    		rlArmColor = 0;
+    		updateColor(testMaterial,rlArmColor);
+    	}
+    	rlArmColor++;
+    }
     GUI.EndGroup();
     
     //Grouping for left arm--------------------------------
-    GUI.BeginGroup(Rect(20, Screen.height/5.5, 130, 110));
+    GUI.BeginGroup(Rect(20, Screen.height/4.5, 130, 140));
     //Left upper arm
     GUI.Label(Rect(0,0, 120, 30),"Upper Left Arm " + luArmSelection.ToString());
     luArmSelection = GUI.HorizontalScrollbar (Rect(0,30, 120, 30),luArmSelection, 1, 1, 8);
@@ -281,7 +346,7 @@ function createCharacter() {
     GUI.EndGroup();
     
     //Grouping for left leg--------------------------------
-    GUI.BeginGroup(Rect(20, Screen.height/2.5, 130, 110));
+    GUI.BeginGroup(Rect(20, Screen.height/2.5, 130, 140));
     //Left upper leg
     GUI.Label(Rect(0, 0, 120, 30),"Upper Left Leg " + luLegSelection.ToString());
     luLegSelection = GUI.HorizontalScrollbar (Rect(0, 30, 120, 30),luLegSelection, 1, 1, 8);
@@ -303,7 +368,7 @@ function createCharacter() {
     GUI.EndGroup();
     
     //Grouping for right leg------------------------------
-    GUI.BeginGroup(Rect(150, Screen.height/2.5, 130, 110));
+    GUI.BeginGroup(Rect(150, Screen.height/2, 130, 140));
     //Right upper leg
     GUI.Label(Rect(0,0, 120, 30),"Upper Right Leg " + ruLegSelection.ToString());
     ruLegSelection = GUI.HorizontalScrollbar (Rect(0,30, 120, 30),ruLegSelection, 1, 1, 8);
@@ -347,6 +412,50 @@ function updateBody(num,part,prevPartNumber)
 	part[prevPartNumber-1].transform.localPosition = Vector3(0,100,100);				
 	part[num-1].transform.localPosition = Vector3(0,0,0);
 	//part[num-1].renderer.material.color = Color.cyan;
+}
+
+function updateColor(matName,colorNumber)
+{
+	if(colorNumber == 0)
+	{
+		matName.SetColor("_Color",Color.black);
+	}
+	else if(colorNumber == 1)
+	{
+		matName.SetColor("_Color",Color.blue);
+	}
+	else if(colorNumber == 2)
+	{
+		matName.SetColor("_Color",Color.gray);
+	}
+	else if(colorNumber == 3)
+	{
+		matName.SetColor("_Color",Color.green);
+	}
+	else if(colorNumber == 4)
+	{
+		matName.SetColor("_Color",Color.magenta);
+	}
+	else if(colorNumber == 5)
+	{
+		matName.SetColor("_Color",Color.red);
+	}
+	else if(colorNumber == 6)
+	{
+		matName.SetColor("_Color",Color.white);
+	}
+	else if(colorNumber == 7)
+	{
+		matName.SetColor("_Color",Color.yellow);
+	}
+	else if(colorNumber == 8)
+	{
+		matName.SetColor("_Color",Color.cyan);
+	}
+	else if(colorNumber == 9)
+	{
+		matName.SetColor("_Color",Color.grey);
+	}
 }
 
 function OnGUI () 
